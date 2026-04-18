7d60b1466c138532502981b1389d68f5d79dcf88
mclaudiarocha <124476560+mclaudiarocha@users.noreply.github.com>
2026-04-17 21:33:57 -0300

    feat: implementa metodo exibirDetalhes na classe Workshop

--- include/Workshop.cpp
+++ include/Workshop.cpp
@@ -4,4 +4,11 @@
 Workshop::Workshop(std::string t, std::string d, int cap, std::string mat, std::string req)
     : Atividade(t, d, cap), listaMateriais(mat), requisitosSoftware(req) {} 
 
-
+void Workshop::exibirDetalhes() const {
+    std::cout << "Workshop: " << std::endl;
+    std::cout << "Título: " << titulo << std::endl;
+    std::cout << "Data: " << data << std::endl;
+    std::cout << "Capacidade: " << capacidadeMaxima << std::endl;
+    std::cout << "Materiais: " << listaMateriais << std::endl;
+    std::cout << "Requisitos: " << requisitosSoftware << std::endl;
+}
