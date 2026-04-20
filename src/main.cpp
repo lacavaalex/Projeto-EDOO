2f2e4ea86ad76a5da5cd14d1742c7e65415a4637
Wallyson Rodrigues <wrswallyson@gmail.com>
2026-04-19 21:29:53 -0300

    feat(main): implementa fluxo de execucao e testes de integracao

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <vector>
-#include "../include/Palestra.hpp" 
-#include "../include/Participante.hpp" 
+#include "../include/Palestra.hpp"
+#include "../include/Workshop.hpp"
+#include "../include/Participante.hpp"
+
+using namespace std;
 
 int main() {
-    // Vetor de ponteiros para a classe base
-    std::vector<Atividade*> eventos;
+    // Cria o Participante 
+    // Eu (Wallyson) escrevi tudo minúsculo e com e-mail estranho para testar o código
+    Participante* usuario = new Participante("wallyson rodrigues", "wallyson@cin.ufpe.br", "Sistemas de Informacao");
 
-    // Adicionando eventos dinamicamente
-    eventos.push_back(new Palestra("Futuro da IA", "15/05/2026", 100, "Dr. Turing"));
-    eventos.push_back(new Palestra("Clean Code", "20/05/2026", 50, "Uncle Bob"));
+    // Cria o "Estoque de Oportunidades" 
+    vector<Atividade*> estoque;
+    estoque.push_back(new Palestra("IA no CIn", "20/04/2026", 2, "Dr. Francisco Simoes"));
+    estoque.push_back(new Workshop("Git Avancado", "22/04/2026", 20, "Monitoria EDOO", "VS Code instalado"));
 
-    std::cout << "--- LISTA DE EVENTOS DO CIN ---\n" << std::endl;
+    cout << "=== BEM-VINDO AO ESTOQUE DE OPORTUNIDADES DO CIN ===\n" << endl;
+    cout << "Usuario logado: " << usuario->getNome() << " (" << usuario->getEmail() << ")" << endl;
 
-    // Percorrendo a lista e exibindo detalhes usando polimorfismo 
-    for (auto e : eventos) {
-        e->exibirDetalhes();
+    // Realizando Inscricoes
+    cout << "\n--- Realizando Inscricoes ---" << endl;
+    for (auto oportunidade : estoque) {
+        oportunidade->seInscrever(usuario);
     }
 
-    // Limpeza de memória
-    for (auto e : eventos) {
-        delete e;
-    }
+    // Verifica o Cronograma do Aluno 
+    cout << "\n--- Meu Cronograma de Atividades ---" << endl;
+    usuario->listarMinhasAtividades();
+
+    // Limpeza de Memoria 
+    for (auto o : estoque) delete o;
+    delete usuario;
 
     return 0;
 }
